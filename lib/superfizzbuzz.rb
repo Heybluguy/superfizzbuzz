10a4af12012872ba2e3250e274115c077210cbbe
Luis  Senior <heybluguy@yahoo.com>
2018-08-20 23:23:03 -0500

    Extracts logic in to methods.

--- lib/superfizzbuzz.rb
+++ lib/superfizzbuzz.rb
@@ -1,3 +1,19 @@
+def supers(i)
+  "Super" if i % 7 == 0
+end
+
+def fizz(i)
+  "Fizz" if i % 3 == 0
+end
+
+def buzz(i)
+  "Buzz" if i % 5 == 0
+end
+
+def outlier(i)
+  i if i % 3 != 0 && i % 5 != 0 && i % 7 != 0
+end
+
 (1..1000).each do |i|
-  puts "#{"Super" if i % 7 == 0}#{"Fizz" if i % 3 == 0}#{"Buzz" if i % 5 == 0}#{i if i % 3 != 0 && i % 5 != 0 && i % 7 != 0}"
+  puts "#{supers(i)}#{fizz(i)}#{buzz(i)}#{outlier(i)}"
 end
